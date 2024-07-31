43c1592b94443ca0e76ef7bddc94d4961474a521
Mohan Saravanan <45027643+saravanan160501@users.noreply.github.com>
2024-07-31 21:34:45 +0800

    Lowered OS Version Requirement

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "OnboardingKit",
     platforms: [
-        .macOS(.v14),
-        .iOS(.v17)
+        .macOS(.v12),
+        .iOS(.v15)
     ],
     products: [
         .library(
